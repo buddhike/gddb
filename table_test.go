309b6ab3f20736d2ef78296dc0a5bf37a3fea5b8
Buddhike de Silva <buddhike@geeksdiary.com>
2026-04-04 16:58:53 +1100

    Test: Test nested types

--- table_test.go
+++ table_test.go
@@ -1,8 +1,6 @@
 package gddb
 
 import (
-	"context"
-	"errors"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -11,9 +9,14 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestEndToEnd(t *testing.T) {
+	ctx := t.Context()
+	client := getDDBClient(t)
+	ensureTable(t, client, "TestEndToEnd", "id", "")
+
 	type Item struct {
 		ID           string  `dynamodbav:"id,omitempty" gddb:"hash"`
 		Name         string  `dynamodbav:"name,omitempty"`
@@ -21,38 +24,10 @@ func TestEndToEnd(t *testing.T) {
 		FencingToken int     `dynamodbav:"fencingToken" gddb:"fence"`
 	}
 
-	ctx := context.Background()
-	cfg, err := config.LoadDefaultConfig(ctx,
-		config.WithRegion("us-east-1"),
-		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("local", "local", "")),
-	)
+	table := NewTable[Item]("TestEndToEnd", client)
 
-	assert.NoError(t, err)
-
-	client := dynamodb.NewFromConfig(cfg, func(o *dynamodb.Options) {
-		o.BaseEndpoint = aws.String("http://localhost:8000")
-	})
-
-	_, err = client.CreateTable(ctx, &dynamodb.CreateTableInput{
-		TableName: aws.String("items"),
-		AttributeDefinitions: []types.AttributeDefinition{
-			{AttributeName: aws.String("id"), AttributeType: types.ScalarAttributeTypeS},
-		},
-		KeySchema: []types.KeySchemaElement{
-			{AttributeName: aws.String("id"), KeyType: types.KeyTypeHash},
-		},
-		BillingMode: types.BillingModePayPerRequest,
-	})
-	if err != nil {
-		var inUse *types.ResourceInUseException
-		if !errors.As(err, &inUse) {
-			assert.NoError(t, err)
-		}
-	}
-
-	table := NewTable[Item]("items", client)
 	{
-		err = DeleteByKey(ctx, table, "item-1")
+		err := DeleteByKey(ctx, table, "item-1")
 		assert.NoError(t, err)
 	}
 
@@ -63,7 +38,7 @@ func TestEndToEnd(t *testing.T) {
 			Price: 10.30,
 		}
 
-		err := InsertUnique(ctx, table, item)
+		err := Insert(ctx, table, item)
 		assert.NoError(t, err)
 	}
 
@@ -114,4 +89,74 @@ func TestEndToEnd(t *testing.T) {
 		assert.Equal(t, 1, l.FencingToken)
 		assert.Equal(t, float32(15), l.Price)
 	}
+}
+
+func TestNestedTypes(t *testing.T) {
+	type address struct {
+		Street   string `dynamodbav:"street"`
+		Suburb   string `dynamodbav:"suburb"`
+		State    string `dynamodbav:"state"`
+		Postcode int    `dynamodbav:"postcode"`
+	}
+	type person struct {
+		ID   string   `dynamodbav:"id" gddb:"hash"`
+		Name string   `dynamodbav:"name"`
+		Home address  `dynamodbav:"address"`
+		Work *address `dynamodbav:"work"`
+	}
+
+	ctx := t.Context()
+	client := getDDBClient(t)
+	ensureTable(t, client, "TestNestedTypes", "id", "")
+
+	tbl := NewTable[person]("TestNestedTypes", client)
+
+	home := address{Street: "66 Anne Street", Suburb: "Sydney", State: "NSW", Postcode: 2000}
+	work := address{Street: "1 George Street", Suburb: "Sydney", State: "NSW", Postcode: 2000}
+	p := person{ID: "alice@corp.com", Name: "Alice", Home: home, Work: &work}
+
+	err := Insert(ctx, tbl, p)
+	assert.NoError(t, err)
+
+	pa, err := FindByKey(ctx, tbl, "alice@corp.com")
+	assert.NoError(t, err)
+	assert.Equal(t, p, pa)
+}
+
+func ensureTable(t *testing.T, client *dynamodb.Client, name string, pk, sk string) {
+	_, err := client.DeleteTable(t.Context(), &dynamodb.DeleteTableInput{TableName: aws.String(name)})
+	if err != nil && !IsErrResourceNotFound(err) {
+		require.NoError(t, err)
+	}
+
+	input := &dynamodb.CreateTableInput{
+		TableName: aws.String(name),
+		AttributeDefinitions: []types.AttributeDefinition{
+			{AttributeName: aws.String("id"), AttributeType: types.ScalarAttributeTypeS},
+		},
+		KeySchema: []types.KeySchemaElement{
+			{AttributeName: aws.String("id"), KeyType: types.KeyTypeHash},
+		},
+		BillingMode: types.BillingModePayPerRequest,
+	}
+	if sk != "" {
+		input.KeySchema = append(input.KeySchema, types.KeySchemaElement{AttributeName: aws.String(sk), KeyType: types.KeyTypeRange})
+	}
+	_, err = client.CreateTable(t.Context(), input)
+	require.NoError(t, err)
+}
+
+func getDDBClient(t *testing.T) *dynamodb.Client {
+	cfg, err := config.LoadDefaultConfig(t.Context(),
+		config.WithRegion("us-east-1"),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("local", "local", "")),
+	)
+
+	assert.NoError(t, err)
+
+	client := dynamodb.NewFromConfig(cfg, func(o *dynamodb.Options) {
+		o.BaseEndpoint = aws.String("http://localhost:8000")
+	})
+
+	return client
 }
